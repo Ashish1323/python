2d6bbdb0ddcf3dab179be94b237d96af3452ade2
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-08-30 23:18:56 +0530

    done password checker

---
+++ Basics/passwordchecker.py
@@ -0,0 +1,4 @@
+user=input("say Your name!!")
+password=input("enter password")
+passs="*" * len(password)
+print(f"Hello {user}, Your Password {passs} is {len(password)} characters long")
---
+++ Basics/strings.py
@@ -0,0 +1,6 @@
+# String Declaration
+newstring="Oh Yeah!!!"
+# length of String
+print(len(newstring))
+# Uppercase the string
+print(newstring.upper())
