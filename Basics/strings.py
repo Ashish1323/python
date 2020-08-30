23c91ae56a47bc2be7a50e4438a8aed05af844fa
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-08-30 23:28:44 +0530

    stings commit

--- Basics/strings.py
+++ Basics/strings.py
@@ -1,6 +1,24 @@
 # String Declaration
 newstring="Oh Yeah!!!"
+
+#Sting Methods...........................
+
 # length of String
 print(len(newstring))
-# Uppercase the string
-print(newstring.upper())
+# Uppercase, Lowercase the string
+print(newstring.upper(), newstring.lower())
+# Find Index of the occuerance
+print(newstring.find(" "))
+
+
+#slicing
+#[start:stop:stepover]
+
+#f formating
+print(f"I love to string {newstring} format!!")
+
+#.format
+print("I love {0}".format(newstring))
+
+
+
