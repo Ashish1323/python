08d32703ba5cd46a4b0a5136059b250a23747a53
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-08-31 21:11:24 +0530

    added conditionals

---
+++ Basics-Loops,Conditions/conditional.py
@@ -0,0 +1,7 @@
+is_old= int(input("Age?????????"))
+is_licenced= input("do you have licence?? answer yes")
+
+if is_old>=18 and is_licenced=="yes":
+    print("you are old enough to drive!!, also you have licence please proceed!!")
+else:
+    print("na na na bhosdk")    
