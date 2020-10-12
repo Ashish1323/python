87c68d5350d4ae92a051d522da41b8f9e25e8d0f
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-10-12 21:02:09 +0530

    map, filter,zip

---
+++ Funtional Programming/filter.py
@@ -0,0 +1,6 @@
+def odd(i):
+    return i%2 !=0
+
+print(list(filter(odd,[1,2,3,4])))    
+
+#output: [1, 3]
---
+++ Funtional Programming/map.py
@@ -0,0 +1,6 @@
+def triple(_):
+    return 3*_
+
+print(list(map(triple,[1,2,3])))
+
+#output : [3, 6, 9]
