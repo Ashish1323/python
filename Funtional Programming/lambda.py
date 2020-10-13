0368f2dd4adf257ddc2814f364d2549bb952fe33
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-10-13 21:05:05 +0530

    lambda function head hurt!!

---
+++ Funtional Programming/lambda.py
@@ -0,0 +1,7 @@
+my_list=[5,10,15]
+
+print(list(map(lambda i: i*i, my_list))) #square bitches
+
+lingo=[(0,2),(4,3),(10,-1)] # sorting a tuple or dictorinary!!!
+lingo.sort(key=lambda x: x[1])
+print(lingo)
