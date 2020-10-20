a44bf2c34423053a66192fd5e7388d222defd51c
Ashish1323 <ashishagnihotri2k@gmail.com>
2020-10-20 19:37:08 +0530

    comprehension excercise bitch!!

---
+++ Funtional Programming/dictcomprehension.py
@@ -0,0 +1,13 @@
+di = {
+    "a": 1,
+    "b": 3
+}
+
+my_dick = {k: v**3 for k, v in di.items()}
+print(my_dick)
+
+# output {'a': 1, 'b': 27}
+
+my_lund = {n: n for n in [1, 2]}
+print(my_lund)
+# output {1: 1, 2: 2}
